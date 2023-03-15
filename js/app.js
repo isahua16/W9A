14ece73b637d4c3b7c9a0e9f937d160943f92ead
isahua16 <isaelhuard@gmail.com>
2023-03-14 21:11:30 -0600

    Added the bonus question answer which uses a compound compound statement

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-let user_age = 17;
-let is_subscribed = false;
+let user_age = 18;
+let is_subscribed = true;
+let user_points = 50;
 
 if (user_age >= 18 && is_subscribed === true) {
     console.log("User is at least 18 years old and is subscribed");
@@ -11,3 +12,6 @@ if (user_age >= 18 && is_subscribed === true) {
     console.log("User is younger than 18 and is not subscribed");
 }
 
+if ((user_age > 18 && is_subscribed === true) || user_points >= 100) {
+    console.log("You are special!");
+}
